d9482bf2e0fad9fb6934ae4f5a85c37770c63a54
eiredrake <eiredrake@gmail.com>
2023-05-30 20:37:20 -0400

    Update models.py
    changed field max-lengths to be integers rather than strings.

--- django_pronouns/models.py
+++ django_pronouns/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Pronoun(models.Model):
-    name = models.CharField(max_length='30')
+    name = models.CharField(max_length=30)
     description = models.CharField(max_length=256)
 
-    subject = models.CharField(max_length='10', help_text="He, she, it")
-    object = models.CharField(max_length='10', help_text="Her, him, it")
-    reflexive = models.CharField(max_length='15', help_text="Itself, himself, herself")
-    possessive_pronoun = models.CharField(max_length='10', help_text="Hers, his, its")
-    possessive_determiner = models.CharField(max_length='10', help_text="His, her, its")
+    subject = models.CharField(max_length=10, help_text="He, she, it")
+    object = models.CharField(max_length=10, help_text="Her, him, it")
+    reflexive = models.CharField(max_length=10, help_text="Itself, himself, herself")
+    possessive_pronoun = models.CharField(max_length=10, help_text="Hers, his, its")
+    possessive_determiner = models.CharField(max_length=10, help_text="His, her, its")
 
     def she_he(self): return self.subject
     def he_she(self): return self.subject
